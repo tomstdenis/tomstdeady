445272022624b9a454adc8d4dab318daeba31fb6
Tom St Denis <tom.stdenis@amd.com>
2024-12-23 19:27:11 -0500

    force languages and fix interleaving bugs in mkv output
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- acomp.sh
+++ acomp.sh
@@ -4,6 +4,8 @@
 RECODE="-c:v libx264 -pix_fmt yuv420p -crf 23 -profile:v baseline -preset fast"
 #otherwise just copy the video out
 COPY="-c:v copy"
+#which audio track to use
+SRCAUD="eng"
 
 if [ "$1" == "-recode" ]; then
        OP="${RECODE}"
@@ -15,14 +17,15 @@ fi
 #make a temp file safely
 TMP=`mktemp`
 #extract audio
-ffmpeg -y -i "${1}" -map 0:a:0 -f s16le -c:a pcm_s16le -ar 48000 -ac 2 ${TMP}
+ffmpeg -y -i "${1}" -map 0:a:m:language:${SRCAUD} -f s16le -c:a pcm_s16le -ar 48000 -ac 2 ${TMP}
 #compress (to 35%)
 tomsteady ${TMP} 35 | tee ${TMP}.log
 #reinsert
 ffmpeg -i "${1}" \
        -f s16le -ar 48000 -ac 2 -i ${TMP} \
        -map 0:v -map 1:a:0 \
-       ${OP} -c:a aac -b:a 128k -map 0:s:0? -c:s copy -map_metadata 0 \
+	-max_interleave_delta 0 \
+       ${OP} -c:a aac -b:a 128k -map 0:s:m:language:${SRCAUD}? -c:s copy -map_metadata 0 \
        "${2}"
 #cleanup
 rm -f ${TMP}
